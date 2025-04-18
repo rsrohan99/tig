b9f679ed2f7342750f0f1f49a44a24caab6cc850
Rohan <rsrohan99@gmail.com>
2025-04-18 19:02:03 +0600

    added llm and tool steps for workflow

--- src/tig/workflows/tig.py
+++ src/tig/workflows/tig.py
@@ -1,5 +1,7 @@
-from typing import List, Any
+import re
+from typing import Dict, List, Any
 
+from xmltodict import parse
 from llama_index.core.llms.llm import LLM
 from llama_index.core.llms import ChatMessage
 from llama_index.core.workflow import (
@@ -11,18 +13,23 @@ from llama_index.core.workflow import (
     StopEvent,
 )
 from tig.modes import MODES
+from tig.prompts.system import get_system_prompt
 
 
 class NewTaskCreated(Event):
     pass
 
 
-class SystemPromptGenerated(Event):
-    system_prompt: str
+class PromptGenerated(Event):
+    prompt: str
 
 
 class LLMResponded(Event):
-    llm_response: str
+    response: str
+
+
+class ToolCallRequired(Event):
+    tool: Dict
 
 
 class TigWorkflow(Workflow):
@@ -39,6 +46,7 @@ class TigWorkflow(Workflow):
             raise ValueError(f"Invalid mode: {mode}")
         self.mode = mode
         self.chat_history: List[ChatMessage] = []
+        self.auto_approve = False
 
     @step
     async def start_new_task(self, ctx: Context, ev: StartEvent) -> NewTaskCreated:
@@ -49,11 +57,33 @@ class TigWorkflow(Workflow):
     @step
     async def generate_system_prompt(
         self, ctx: Context, ev: NewTaskCreated
-    ) -> SystemPromptGenerated:
+    ) -> PromptGenerated:
         task = await ctx.get("task")
-        mode = MODES[self.mode]
+        system_prompt = get_system_prompt(self.mode, task)
+        return PromptGenerated(prompt=system_prompt)
 
     @step
-    async def prompt_llm(self, ctx: Context, ev: NewTaskCreated) -> LLMResponded:
-        task = await ctx.get("task")
-        mode = MODES[self.mode]
+    async def prompt_llm(self, ev: PromptGenerated) -> LLMResponded:
+        prompt = ev.prompt
+        self.chat_history.append(ChatMessage(role="user", content=prompt))
+        response = await self.llm.achat(messages=self.chat_history)
+        self.chat_history.append(ChatMessage(role="assistant", content=str(response)))
+        return LLMResponded(response=str(response))
+
+    @step
+    async def handle_response(
+        self, ev: LLMResponded
+    ) -> ToolCallRequired | PromptGenerated:
+        response = ev.response
+        pattern = r"<(?P<tag>\w+)[^>]*>.*</(?P=tag)>"
+        match = re.search(pattern, response, re.DOTALL)
+        if not match:
+            return PromptGenerated(
+                prompt="You did not use any tool, please use appropriate tool using valid xml format."
+            )
+        tool = parse(match.group())
+        return ToolCallRequired(tool=tool)
+
+    @step
+    async def use_tool(self, ctx: Context, ev: ToolCallRequired) -> PromptGenerated | StopEvent:
+
